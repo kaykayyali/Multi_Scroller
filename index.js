0e8ce1e2781b9e2cc5f876bf1535644c2c968071
Kaysser Emad Kayyali <kaykayyali@gmail.com>
2017-03-18 17:17:27 -0400

    Detect heroku port

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function(req, res){
 //   	});
 // });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
 

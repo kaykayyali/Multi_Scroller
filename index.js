4a85e9bba6416f648f3e01deb942c8188fc237cf
Kaysser Emad Kayyali <kaykayyali@gmail.com>
2017-03-18 17:15:11 -0400

    Create npm start filep

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var User = require('./lib/server_user');
-var User_Cache = require('./lib/server_user_cache');
 
-var user_cache = new User_Cache();
 
 app.use(express.static('public'))
 
@@ -12,13 +10,13 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(socket){
-  	var new_user = new User(socket.id);
-  	socket.emit('user_created', new_user);
-  	socket.on('disconnect', function(data){
-   		console.log('user disconnected', data);
-  	});
-});
+// io.on('connection', function(socket){
+//   	var new_user = new User(socket.id);
+//   	socket.emit('user_created', new_user);
+//   	socket.on('disconnect', function(data){
+//    		console.log('user disconnected', data);
+//   	});
+// });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');

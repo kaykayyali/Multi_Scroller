9e7000a561484e63828dde2d9bf7aae02038b858
Kaysser Emad Kayyali <kaykayyali@gmail.com>
2017-03-18 19:16:02 -0400

    Got one player working

--- public/js/player.js
+++ public/js/player.js
@@ -1,25 +1,56 @@
-var Player = function(){
+var Player = function(state){
+	// Transfers Game state so we can use the keys already assigned there.
+	this.state = state;
 	this.sprite = Game_Client.game.add.sprite(100, 400, 'dude');
 	this.sprite.animations.add('left', [0, 1, 2, 3], 10, true);
 	this.sprite.animations.add('turn', [4], 20, true);
 	this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
 	this.sprite.animations.play('right');
 	this.run_speed = 5;
-	this.jump_power = 100;
+	this.jump_power = -500;
 	this.facing = 'left';
 	this.jump_timer = 0;
 };
 
 Player.prototype.set_physics = function() {
-	Game_Client.game.physics.enable([this.sprite], Phaser.Physics.ARCADE);
+	Game_Client.game.physics.arcade.enable(this.sprite);
 	this.sprite.body.collideWorldBounds = true;
 	this.sprite.body.bounce.y = 0.1;
 	this.sprite.body.friction = 10;
-	this.sprite.body.onCollide = new Phaser.Signal()
-	this.sprite.body.onCollide.add(this.handle_collide, this);
 };
 
-Player.prototype.handle_collide = function(object) {
-	console.log("Called");
-	this.last_jump = false;
+Player.prototype.update = function() {
+	Game_Client.game.physics.arcade.collide(this.sprite, this.state.platforms, this.state.setFriction, null, this);
+	this.standing = this.sprite.body.blocked.down || this.sprite.body.touching.down;
+	this.sprite.body.velocity.x = 0;
+	if (this.state.cursors.left.isDown) {
+		this.sprite.body.velocity.x = -150;
+		if (this.facing != 'left') {
+			this.sprite.animations.play('left');
+			this.facing = 'left';
+		}
+	}
+	else if (this.state.cursors.right.isDown) {
+		this.sprite.body.velocity.x = 150;
+		if (this.facing != 'right') {
+			this.sprite.animations.play('right');
+			this.facing = 'right';
+		}
+	}
+	else {
+		if (this.facing != 'idle') {
+			this.sprite.animations.stop();
+			if (this.facing == 'left') {
+				this.sprite.frame = 0;
+			} 
+			else {
+				this.sprite.frame = 5;
+			}
+			this.facing = 'idle';
+		}
+	}
+	if (this.state.jump_key.isDown && this.standing) {
+		this.sprite.body.velocity.y = this.jump_power;
+		this.jump_timer = Game_Client.game.time.now + 750;
+	}
 };
--- public/js/states/game.js
+++ public/js/states/game.js
@@ -10,42 +10,13 @@ var Game_State = {
 		this.set_environment();
 		this.create_background();
 		this.create_main_player();
+		this.create_platforms();
 		this.set_physics();
 		this.assign_keys();
 	},
 	update: function() {
-		this.player.sprite.body.velocity.x = 0;
-		if (this.cursors.left.isDown) {
-			this.player.sprite.body.velocity.x = -150;
-			if (this.player.facing != 'left') {
-				this.player.sprite.animations.play('left');
-				this.player.facing = 'left';
-			}
-		}
-		else if (this.cursors.right.isDown) {
-			this.player.sprite.body.velocity.x = 150;
-			if (this.player.facing != 'right') {
-				this.player.sprite.animations.play('right');
-				this.player.facing = 'right';
-			}
-		}
-		else {
-			if (this.player.facing != 'idle') {
-				this.player.sprite.animations.stop();
-				if (this.player.facing == 'left') {
-					this.player.sprite.frame = 0;
-				} 
-				else {
-					this.player.sprite.frame = 5;
-				}
-				this.player.facing = 'idle';
-			}
-		}
-		if (this.jump_key.isDown && this.player.sprite.body.onFloor() && Game_Client.game.time.now > this.player.jump_timer) {
-			this.player.sprite.body.velocity.y = -500;
-			this.player.jumpTimer = Game_Client.game.time.now + 750;
-		}
-		console.log('Frame')
+		this.player.update();
+		this.update_platforms();
 	},
 	assign_keys: function() {
 		this.cursors = Game_Client.game.input.keyboard.createCursorKeys();
@@ -55,14 +26,42 @@ var Game_State = {
 		this.background = Game_Client.game.add.tileSprite(0, 0, 640, 480, 'background');
 	},
 	create_main_player: function() {
-		this.player = new Player();
+		this.player = new Player(this);
+	},
+	create_platforms: function() {
+		this.platforms = this.add.physicsGroup();
+		this.platforms.create(400, 412, 'platform');
+		this.platforms.create(300, 352, 'platform');
+		this.platforms.create(200, 296, 'platform');
+		this.platforms.create(100, 180, 'platform');
+		this.platforms.create(0, 64, 'platform');
+		this.platforms.setAll('body.allowGravity', false);
+		this.platforms.setAll('body.immovable', true);
+		this.platforms.setAll('body.velocity.x', 100);
 	},
 	set_environment: function() {
+		this.platforms = [];
 		Game_Client.game.physics.arcade.gravity.y = 1000;
 	},
 	set_physics: function() {
 		var self = this;
 		console.log(this)
 		this.player.set_physics();
-	}
+	},
+	setFriction: function (player, platform) {
+		if (platform.key === 'ice-platform') {
+			player.body.x -= platform.body.x - platform.body.prev.x;
+		}
+	},
+	update_platforms: function() {
+		this.platforms.forEach(this.wrap_platform, this);
+	},
+	wrap_platform: function (platform) {
+		if (platform.body.velocity.x < 0 && platform.x <= -160) {
+		    platform.x = 640;
+		}
+		else if (platform.body.velocity.x > 0 && platform.x >= 640) {
+		    platform.x = -160;
+		}
+	},
 };
